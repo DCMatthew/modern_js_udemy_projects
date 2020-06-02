5887c77cef975237b2d4dd7fe170ac7e08558f55
David Matthew <david@matthewenterprises.net>
2020-06-02 00:20:39 -0400

    created nested object

--- 1.js_sandbox/app.js
+++ 1.js_sandbox/app.js
@@ -1,7 +1,15 @@
 const feral = {
 	firstName: "David",
 	lastName: "Matthew",
-	occupation: "IT-Operations",
-	isMarried: true
+	isMarried: true,
+	languages: {
+		JavaScript: "Fundamentals",
+		HTML: "Advanced",
+		CSS: "Advanced",
+		PHP: "Fundamentals",
+	},
+	currentOccupation: "IT-Operations",
+	desiredOccupation: "Developer",
 };
-console.table(feral);
+
+console.table(feral.languages);
